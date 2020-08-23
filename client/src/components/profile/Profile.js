9937c87263626b185e5b730c6092727db17590c7
SilvenLEAF <SilvenLEAF@gmail.com>
2020-08-23 17:39:20 +0530

    added update and delete buttons on profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,7 @@ import M from 'materialize-css'
 import '../../styles/Profile.scss'
 
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 function Profile() {
   useEffect(()=>{
@@ -20,6 +21,12 @@ function Profile() {
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ducimus minima perspiciatis deserunt sapiente dolorem at hic accusamus iure totam iste dolore natus perferendis maiores, asperiores, quaerat nam nobis, minus fugit? Animi eligendi nesciunt earum facilis ipsum, atque aspernatur. Architecto cupiditate impedit eveniet cum expedita dolor facere inventore officia quam.
       </p>
+
+
+      <div className="right myProfileBtnsHolder">
+        <Link to="/" className="btn blue" style={{marginRight: "20px"}}>Update</Link>
+        <Link to="/" className="btn red darken-4">Delete</Link>
+      </div>
     </div>
   )
 }
