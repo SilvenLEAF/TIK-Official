124a37649b32f2c23b04522f8ab0dda87ef3ef4b
SilvenLEAF <SilvenLEAF@gmail.com>
2020-08-24 13:41:07 +0530

    restored other oauth keys

--- config/oauthKeys.js
+++ config/oauthKeys.js
@@ -1,30 +1,30 @@
 const AMAZON = {
-  clientID: "",
-  clientSecret: ""
+  clientID: "amzn1.application-oa2-client.8c69d80c4d3b441e95bb0dea68412103",
+  clientSecret: "0670646811fceac12bce8eb45572a9f978e80b000818f1f4cf867cfe5e9d1c27"
 };
 const GITHUB = {
-  clientID: "",
-  clientSecret: ""
+  clientID: "ca07c74962097b8480f6",
+  clientSecret: "a9428e7a75c59e24a32c0f473da2dbd42d5fbb89"
 };
 const FACEBOOK = {
-  clientID: "",
-  clientSecret: ""
+  clientID: "2503599789703821",
+  clientSecret: "645088b310270cf3abbda58e01f65e96"
 };
 const GOOGLE = {
   clientID: "948666598374-tj740dlbnbt9dmtvi6k1lifi16ml13mj.apps.googleusercontent.com",
   clientSecret: "7kl5vertxRo6C8ezBT0YLqp-"
 };
 const INSTAGRAM = {
-  clientID: "",
-  clientSecret: ""
+  clientID: "0d517fd22d63445181091e63578caf92",
+  clientSecret: "3ea8588c8f7f43ebb4ff89fd5519b1fc"
 };
 const SPOTIFY = {
-  clientID: "",
-  clientSecret: ""
+  clientID: "2ab04beb997b4e75bede51e1caed6ffc",
+  clientSecret: "27d00d3b6dcb4ac2af1fa6f64cd0ce74"
 };
 const TWITCH = {
-  clientID: "",
-  clientSecret: ""
+  clientID: "3bim2h9k5ht5jfyktqpuqiasdexg27",
+  clientSecret: "bz4tt9sztacz4sd8qx8rg8imqbsjjn"
 };
 
 module.exports = {
